368f2ccc248b4a95f9d59fff6c9cc164087d256d
Jestoni Yap <jestoni@ingenuity.com>
2015-01-14 17:30:09 +0800

    login feature exercise

--- ingenWitty/LoginViewController.swift
+++ ingenWitty/LoginViewController.swift
@@ -25,28 +25,7 @@ class LoginViewController: UIKit.UIViewController {
     
     // Event Handlers
     @IBAction func loginUser(sender : AnyObject) {
-        if usernameField.text.isEmpty || passwordField.text.isEmpty {
-            self.errorLabel.text = "Please fill up all the fields"
-        }else{
-            self.errorLabel.text = ""
-            var params = [String: String]()
-            params["username"] = self.usernameField.text
-            params["password"] = self.passwordField.text
-            
-            Client.authenticateWithCredentials(params, completion:{(data, error) -> Void in
-                if (data != nil){
-                    dispatch_async(dispatch_get_main_queue()) {
-                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                        let vc = storyboard.instantiateViewControllerWithIdentifier("feedViewController") as UIViewController
-                        self.presentViewController(vc, animated: true, completion: nil)
-                    }
-                }else{
-                    dispatch_async(dispatch_get_main_queue()) {
-                        self.errorLabel.text = "Login Failed"
-                    }
-                }
-            })
-        }
+        
     }
     
 }
